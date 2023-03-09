57ab1f8ac762f89524be66ad84a0c085ce390933
HungNguyen1213 <52905968+HungNguyen1213@users.noreply.github.com>
2023-03-09 09:48:49 +0700

    Protected route & navbar component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,30 @@
-import Head from "next/head";
-import Image from "next/image";
-import { Inter } from "next/font/google";
-import styles from "@/styles/Home.module.css";
+import { NextPageContext } from "next";
+import { getSession } from "next-auth/react";
 
-const inter = Inter({ subsets: ["latin"] });
+import useCurrentUser from "@/hooks/useCurrentUser";
+import { Navbar } from "@/components";
+
+export async function getServerSideProps(context: NextPageContext) {
+  const session = await getSession(context);
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/auth",
+        permanent: false,
+      },
+    };
+  }
+
+  return { props: {} };
+}
 
 export default function Home() {
+  const { data: user } = useCurrentUser();
+
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Netflix clone</h1>
+      <Navbar />
     </>
   );
 }
