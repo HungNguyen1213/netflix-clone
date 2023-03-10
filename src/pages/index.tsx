1b77c41c04d66be40ddb2afd0b9ae433d7dff0ca
HungNguyen1213 <52905968+HungNguyen1213@users.noreply.github.com>
2023-03-10 09:41:27 +0700

    Info modal

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,10 @@
 import { NextPageContext } from "next";
 import { getSession } from "next-auth/react";
 
-import useCurrentUser from "@/hooks/useCurrentUser";
-import { Navbar, Billboard, MovieList } from "@/components";
+import { Navbar, Billboard, MovieList, InfoModal } from "@/components";
 import useMovieList from "@/hooks/useMovieList";
 import useFavorites from "@/hooks/useFavorites";
+import useInfoModalStore from "@/hooks/useInfoModalStore";
 
 export async function getServerSideProps(context: NextPageContext) {
   const session = await getSession(context);
@@ -22,12 +22,13 @@ export async function getServerSideProps(context: NextPageContext) {
 }
 
 export default function Home() {
-  const { data: user } = useCurrentUser();
+  const { isOpen, closeModal } = useInfoModalStore();
   const { data: movies = [] } = useMovieList();
   const { data: favorites = [] } = useFavorites();
 
   return (
     <>
+      <InfoModal visible={isOpen} onClose={closeModal} />
       <Navbar />
       <Billboard />
       <div className="pb-40">
